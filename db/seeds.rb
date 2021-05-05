00ba0a297749518f926fa0377b7a990ba4829ed1
Kyle Danny <codingkyle92@gmail.com>
2021-05-05 15:46:33 +0200

    mvc structure complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+puts "Database is clean"
+
+puts "Creating Flats..."
+
+4.times do
+  flat = Flat.create(
+    name: Faker::Artist.name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: [65, 85, 110, 90].sample,
+    number_of_guests: rand(1..5),
+    picture_url: "https://source.unsplash.com/random/?house"
+    )
+  puts "flat #{flat.id} is created"
+end
+
+puts "Database populated!"
